6705ec85d6b43a61cde3d66e8a8b6555e10fa2bf
YASHVI <yashvivanza01@gmail.com>
2025-06-23 17:30:54 +0530

    Initial commit

---
+++ [35] FInal Keyword.py
@@ -0,0 +1,30 @@
+try:
+    l = [1,5,6,7]
+    i = int(input("Enter the index :- "))
+    print(l[i])
+except:
+    print("Some error occured")
+finally:
+    print("I am always exceuted")
+
+
+# Difference between directly printing or use of finally keyword, gives same output.
+# So using function we will see
+
+print("  ")
+
+def funct1():
+    try:
+        l = [1,5,6,7]
+        i = int(input("Enter the index :- "))
+        print(l[i])
+        return 1
+    except:
+        print("Some error occured")
+        return 0
+    finally:
+        print("I am always exceuted")
+    #print("I am always exceuted")
+
+x = funct1()
+print(x)

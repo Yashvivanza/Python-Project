410844d91623e7ab1883d208dbf1d5ed4b4c1b8c
YASHVI <yashvivanza01@gmail.com>
2025-06-24 19:34:15 +0530

    Initial commit

---
+++ [39] Virtual Environment.py
@@ -0,0 +1,17 @@
+# A Virtual environment is a tool used to indicate specific Python environments on a single machine , allowing us to work on multiple projects with different dependencies and packages without conflicts.
+
+# This can be especially useful when working on projects that have conflicting package versions or package that are not compatible with each other.
+
+'''
+
+    Create a virtual environment:-
+    python -m venv myenv
+
+    Activate the virtual environment (Windows):-
+    myenv\Scripts\activate.bat
+'''
+
+'''
+    Deactivate the virtual environment:-
+    deactivate
+'''

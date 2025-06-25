99dc31909c0d981c7f511f9a34a469352a233ee2
YASHVI <yashvivanza01@gmail.com>
2025-06-25 19:08:17 +0530

    Initial commit

---
+++ [41] Idiom if __name.py
@@ -0,0 +1,11 @@
+def welcome():
+    print("Hey you are welcome from yashvi")
+if __name__ == '__main__':
+    welcome()
+
+print(" ")
+
+def main():
+    print("Running script directly")
+if __name__ == '__main__':
+    main()

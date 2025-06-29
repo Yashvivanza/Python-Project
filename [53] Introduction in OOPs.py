1e54ad14c4821d56fd5b956afb3b7add22ea000e
YASHVI <yashvivanza01@gmail.com>
2025-06-29 22:01:56 +0530

    Initial Commit

---
+++ [53] Introduction in OOPs.py
@@ -0,0 +1,7 @@
+'''
+    1. Procedural Programming
+    2. Object-Orientd Programming
+'''
+# OOP in Python is to use classes and objects to represent real - world concepts and entities.
+
+# A class is a blueprit for creating the objects.

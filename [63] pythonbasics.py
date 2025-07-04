f67031e8353434450e280913f0db167b20dfeda7
YASHVI <yashvivanza01@gmail.com>
2025-07-04 22:00:12 +0530

    Initial commit

---
+++ [63] pythonbasics.py
@@ -0,0 +1,23 @@
+3 + 4
+#%%
+3-5
+#%%
+2*2*2
+#%%
+2 ** 3
+#%%
+print('sanjay')
+#%%
+print("sanjay")
+#%%
+print("sanjay's notebook")
+#%%
+print('sanjay "notrebook"')
+#%%
+'sam' + 'sam'
+#%%
+print('\nsam' * 10)
+#%%
+# prompt: print sam 10 times and each on new line without using loop
+
+print('sam\n' * 10)

200e208c53e60be9f883efb2ed6d6e063dd70ff1
YASHVI <yashvivanza01@gmail.com>
2025-07-04 22:05:16 +0530

    Initial commit

---
+++ [64] Practice.py
@@ -0,0 +1,57 @@
+# to print raw string
+
+
+print(r'c:\turboc\nbgi')
+#%%
+x=3
+x+3
+#%%
+y=3
+x+y
+#%%
+x+10
+#%%
+_+y
+#%%
+name = 'sanjay'
+name
+#%%
+name + ' prajapati'
+#%%
+name
+#%%
+name[0]
+name[6]
+#%%
+name[-1]
+#%%
+name[-2]
+#%%
+name[0:2]
+#%%
+name[1:4]
+#%%
+name[1:]
+#%%
+name[3:10]
+#%%
+name[0] = 'a'
+#%%
+len(name)
+#%%
+#LIST - the are mutable
+
+nums = [25,22,15,12,10]
+nums
+
+#%%
+nums[0]
+#%%
+nums[4]
+#%%
+nums[2:]
+#%%
+nums[-1]
+#%%
+names = ['sam','pan','meg']
+names
